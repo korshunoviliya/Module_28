6de7a12a43061b8bbdaea4e02618f421d3fab2da
korshunoviliya <korshunov80@mail.ru>
2022-01-29 16:35:09 +0300

    create project

---
+++ module_28_2/cmake-build-debug/CMakeFiles/module_28_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_28_2.dir/main.cpp.obj"
+  "libmodule_28_2.dll.a"
+  "module_28_2.exe"
+  "module_28_2.exe.manifest"
+  "module_28_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module_28_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
