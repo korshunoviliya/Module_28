9a17a273012c59f367822747f28162daf316321e
korshunoviliya <korshunov80@mail.ru>
2022-01-29 19:49:28 +0300

    task #2 is ready

--- module_28_2/main.cpp
+++ module_28_2/main.cpp
@@ -3,7 +3,7 @@
 #include <mutex>
 #include <vector>
 
-bool isBusy{false};
+static bool isBusy{false};
 std::mutex wait_free_platform;
 
 class Train{
@@ -37,36 +37,40 @@ void wait_to_arrive(Train train){
     std::cout << "The train " << train.getName() << " pulled up to the station" << std::endl;
     if (isBusy) {
         std::cout << "The train " << train.getName() << " is waiting for a free platform" << std::endl;
-        wait_free_platform.lock();
-    } else {
-        std::cout << "The train " << train.getName() << " arrived at the station" << std::endl;
-        isBusy = true;
-        std::string command;
-        while (command != "d"){
-            std::cin >> command;
-        }
-        std::cout << "the train " << train.getName() << " left the station" << std::endl;
-        isBusy = false;
-        wait_free_platform.unlock();
     }
+    wait_free_platform.lock();
+    std::cout << "The train " << train.getName() << " arrived at the station" << std::endl;
+    isBusy = true;
+    std::string command;
+    while (command != "d"){
+    std::cin >> command;
+    }
+    std::cout << "the train " << train.getName() << " left the station" << std::endl;
+    isBusy = false;
+    wait_free_platform.unlock();
 }
 
-
 int main() {
-    Train *train = new Train[3];
-    train[0].setName('A');
-    train[1].setName('B');
-    train[2].setName('C');
-    train[0].setTravelTime(2);
-    train[1].setTravelTime(6);
-    train[2].setTravelTime(4);
+    const int numberTrains = 3;
+    Train *trains = new Train[numberTrains];
+    trains[0].setName('A');
+    trains[1].setName('B');
+    trains[2].setName('C');
+    int time;
+    for (size_t i = 0; i < numberTrains; ++i){
+        std::cout << "Enter the travel time to the station for the trains " <<
+                  trains[i].getName() << ":";
+        std::cin >> time;
+        trains[i].setTravelTime(time);
+    }
 
     std::vector<std::thread> trainTraffic;
     for(size_t i = 0; i < 3; ++i) {
-        trainTraffic.emplace_back(wait_to_arrive, train[i]);
+        trainTraffic.emplace_back(wait_to_arrive, trains[i]);
     }
     trainTraffic[0].join();
     trainTraffic[1].join();
     trainTraffic[2].join();
+    delete[] trains;
     return 0;
 }
