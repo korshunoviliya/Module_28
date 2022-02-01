abb1925a875537d6abf9256866e01392bb9f3bdc
korshunoviliya <korshunov80@mail.ru>
2022-02-01 19:42:15 +0300

    create project

--- module_28_1/main.cpp
+++ module_28_1/main.cpp
@@ -4,94 +4,104 @@
 #include <mutex>
 #include <algorithm>
 
-const double max_distance = 50;
-std::mutex swim_access;
-
-class Swimmer{
-private:
-    std::string name{"none"};
-    double speed{0.0};
+struct Swimmer
+{
     double total_time{0};
-public:
-    Swimmer() = default;
-    Swimmer(std::string inName, double inSpeed) : name(inName), speed(inSpeed){}
-
-    const std::string &getName() const {
-        return name;
-    }
-
-    double getSpeed() const {
-        return speed;
-    }
-
-    double getTotalTime() const {
-        return total_time;
-    }
-
-    void setTotalTime(double totalTime) {
-        Swimmer::total_time = totalTime;
+    double speed{0};
+    std::string name{"none"};
+    Swimmer(std::string name, double speed){
+        this->name = name;
+        this->speed = speed;
     }
 };
 
-void startSwim(Swimmer* swimmer)
+class Swim
 {
-    double timer{0};
-    double distance = max_distance;
-    while(distance != 0) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+private:
+    std::vector<Swimmer*> result;
+    const int swimmingLane = 6;
+    Swimmer *swimmer;
+    std::vector<Swimmer*> swimmers;
+    std::vector<std::thread> swim;
+    std::mutex swim_access;
+public:
+    Swim() = default;
+
+    void printResult(){
+        std::cout << "*******************************" << std::endl;
+        for (int i = 0; i < result.size(); ++i){
+            std::cout << i + 1 << " Name:" << result[i]->name <<
+                      " Total time:" << result[i]->total_time << " sec." << std::endl;
+        }
+        std::cout << "*******************************" << std::endl;
+    }
+
+    void startSwim(Swimmer *pSwimmer){
+        const double max_distance = 20;
+        double timer{0};
+        double distance = max_distance;
+
+        while(distance != 0) {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            swim_access.lock();
+            distance -= pSwimmer->speed;
+            ++timer;
+            if (distance <= 0) {
+                distance = 0;
+                std::cout << pSwimmer->name << " FINISH!" << std::endl;
+            }
+            else {
+                std::cout << pSwimmer->name << " swam " << max_distance - distance << " meters." << std::endl;
+            }
+            swim_access.unlock();
+        }
+        pSwimmer->total_time = timer;
+
         swim_access.lock();
-        distance -= swimmer->getSpeed();
-        ++timer;
-        if (distance < 0) distance = 0;
-        std::cout << swimmer->getName() << " swam " << max_distance - distance << " meters." << std::endl;
+        result.push_back(pSwimmer);
         swim_access.unlock();
     }
-    swim_access.lock();
-    swimmer->setTotalTime(timer);
-    swim_access.unlock();
-}
 
-bool comp(Swimmer *a,Swimmer *b){
-    return a->getTotalTime() < b->getTotalTime();
-}
+    void init(){
+        for(size_t i = 0; i < swimmingLane; ++i) {
+            std::string swimmerName;
+            std::cout << "Enter the name of the swimmer:";
+            std::cin >> swimmerName;
+            double swimmerSpeed;
+            std::cout << "Enter the speed of the swimmer:";
+            std::cin >> swimmerSpeed;
+            swimmer = new Swimmer(swimmerName, swimmerSpeed);
+            swimmers.push_back(swimmer);
+        }
+    }
+    void initConst(){
+        swimmer = new Swimmer{"ivan", 1.8};
+        swimmers.push_back(swimmer);
+        swimmer = new Swimmer{"alf", 1.5};
+        swimmers.push_back(swimmer);
+        swimmer = new Swimmer{"petr", 2.1};
+        swimmers.push_back(swimmer);
+    }
+
+    void start(){
+
+        for(auto & swimmer : swimmers) {
+            swim.emplace_back(&Swim::startSwim, this, swimmer);
+        }
+        for(auto & i : swim) {
+            i.join();
+        }
+    }
+};
 
 int main() {
-    const int swimmingLane = 6;
-    std::vector<Swimmer*> swimmers;
-    Swimmer *swimmer;
+    Swim *pSwim = new Swim;
 
-    swimmer = new Swimmer{"ivan", 1.8};
-    swimmers.push_back(swimmer);
-    swimmer = new Swimmer{"alf", 1.5};
-    swimmers.push_back(swimmer);
-    swimmer = new Swimmer{"petr", 2.1};
-    swimmers.push_back(swimmer);
-    /*
-    for(size_t i = 0; i < swimmingLane; ++i) {
-        std::string swimmerName;
-        std::cout << "Enter the name of the swimmer:";
-        std::cin >> swimmerName;
-        double swimmerSpeed;
-        std::cout << "Enter the speed of the swimmer:";
-        std::cin >> swimmerSpeed;
-        swimmer = new Swimmer(swimmerName, swimmerSpeed);
-        swimmers.push_back(swimmer);
-    }*/
-    std::vector<std::thread> swim;
-    for(size_t i = 0; i < swimmers.size(); ++i) {
-        swim.emplace_back(startSwim, swimmers[i]);
-    }
-    for(size_t i = 0; i < swim.size(); ++i) {
-        swim[i].join();
-    }
-    std::sort(swimmers.begin(), swimmers.end(), comp);
+    //pSwim->init();
+    pSwim->initConst();
+    pSwim->start();
+    pSwim->printResult();
 
-    std::cout << "*******************************" << std::endl;
-    for(size_t i = 0; i < swimmers.size(); ++i) {
-
-        std::cout << i + 1 << " Name:" << swimmers[i]->getName() <<
-                    " Total time:" << swimmers[i]->getTotalTime() << " sec." << std::endl;
-    }
-    delete swimmer;
+    delete pSwim;
     return 0;
 }
