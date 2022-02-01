1cfb8bc2ed29a844d1ede794f9f987b9f1d6401e
korshunoviliya <korshunov80@mail.ru>
2022-02-01 10:45:46 +0300

    modified to reflect curator's comments and add struct OrderType

--- module_28_3/main.cpp
+++ module_28_3/main.cpp
@@ -10,9 +10,9 @@ int getRandom(int min, int max) {
     std::uniform_int_distribution<> uid(min, max);
     return uid(gen);
 }
-/*
+
 struct OrderType{
-    int orderNumber;
+    int orderNumber{0};
     int dish;
 
     enum Dish {
@@ -24,7 +24,6 @@ struct OrderType{
     };
 
     static std::string typeDish(int dish) {
-
         if (dish == PIZZA) return "Pizza";
         else if (dish == SOUP) return "Soup";
         else if (dish == STEAK) return "Steak";
@@ -38,37 +37,18 @@ std::ostream& operator<< (std::ostream &out, const OrderType &orderType){
     out << "#" << orderType.orderNumber << " - " << OrderType::typeDish(orderType.dish);
     return out;
 }
-*/
+
 class Kitchen {
     static int numberOfDeliveries;
     bool kitchenIsFree{true};
     const int timeOfDelivery = 30;
     const int totalDelivery = 10;
 
-    std::queue<int> orderQueue;
-    std::queue<int> deliveryQueue;
+    std::queue<OrderType> orderQueue;
+    std::queue<OrderType> deliveryQueue;
 
     std::mutex wait_orderQueue;
     std::mutex wait_deliveryQueue;
-
-    //friend struct OrderType;
-
-    enum Dish {
-        PIZZA,
-        SOUP,
-        STEAK,
-        SALAD,
-        SUSHI,
-    };
-
-    std::string typeDish(int dish) {
-
-        if (dish == PIZZA) return "Pizza";
-        else if (dish == SOUP) return "Soup";
-        else if (dish == STEAK) return "Steak";
-        else if (dish == SALAD) return "Salad";
-        else return "Sushi";
-    }
 
     void delivery(){
         while (totalDelivery > numberOfDeliveries) {
@@ -80,7 +60,7 @@ class Kitchen {
                 int sizeDeliveryQueue = deliveryQueue.size();
                 for(int i = 0; i < sizeDeliveryQueue; ++i){
                     std::cout << "\t" << i + 1 << ")"
-                              << typeDish(deliveryQueue.front()) << std::endl;
+                              << deliveryQueue.front() << std::endl;
                     wait_deliveryQueue.lock();
                     deliveryQueue.pop();
                     wait_deliveryQueue.unlock();
@@ -95,32 +75,31 @@ class Kitchen {
             if (kitchenIsFree && !orderQueue.empty()) {
                 kitchenIsFree = false;
                 wait_orderQueue.lock();
-                int temp = orderQueue.front();
+                OrderType temp = orderQueue.front();
                 orderQueue.pop();
                 wait_orderQueue.unlock();
                 std::cout << "The order has been sent to the kitchen: "
-                          << typeDish(temp) << std::endl;
+                          << temp << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(getRandom(5, 15)));
-                std::cout << "The order: " << typeDish(temp)
+                std::cout << "The order: " << temp
                           << " is ready!" << std::endl;
-
 
                 wait_deliveryQueue.lock();
                 deliveryQueue.push(temp);
                 wait_deliveryQueue.unlock();
                 kitchenIsFree = true;
             }
-
         }
     }
 
     void order() {
+        OrderType newOrder;
         while (totalDelivery > numberOfDeliveries){
-
-
             std::this_thread::sleep_for(std::chrono::seconds(getRandom(5,10)));
-            int newOrder = getRandom(0,4);
-            std::cout << "New order accepted: " << typeDish(newOrder) << std::endl;
+            newOrder.dish= getRandom(0,4);
+            ++newOrder.orderNumber;
+            if (newOrder.orderNumber == 100) newOrder.orderNumber = 1;
+            std::cout << "New order accepted: " << newOrder << std::endl;
 
             wait_orderQueue.lock();
             orderQueue.push(newOrder);
